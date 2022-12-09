8f79ef2ec96f17fc81610a46a108fc05d147b9ad
XeNoXTS <palmyunvisate2003@gmail.com>
2022-12-09 16:05:25 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
+    int even = 0;
+    int odd = 0;
+    int g=1;
+    while(g != 0){
+        int N;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+    g = N;
+        if(N%2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+    cout << "#Even numbers = " << even-1 << endl;
+    cout << "#Odd numbers = " << odd << endl;
     return 0;
 }
